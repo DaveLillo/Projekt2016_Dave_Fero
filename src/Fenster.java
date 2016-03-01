a5fb423d2db0d8b42634de0b8636be62ea46439a
DaveLillo <wur17091@spengergasse.at>
2016-03-01 09:30:15 +0100

    Fenster Klasse + Testklasse hinzugefügt

---
+++ src/Fenster.java
@@ -0,0 +1,114 @@
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Fenster {
+
+	private int height;
+	private int width;
+	private Color backgroundColor;
+	private boolean centered, fullScreen;
+	private JFrame fenster;
+
+	public Fenster() {
+		height = getScreenHeight();
+		width = getScreenWidth();
+		backgroundColor = Color.BLACK;
+		centered = true;
+		fenster = new JFrame();
+		setFullScreen(true);
+	}
+
+	public Fenster(int width, int height, Color backgroundColor, boolean centered, boolean fullScreen) {
+		setHeight(height);
+		setWidth(width);
+		setBackgroundColor(backgroundColor);
+		setCentered(centered);
+		setFullScreen(fullScreen);
+		fenster = new JFrame();
+	}
+	
+	public boolean getFullScreen() {
+		return fullScreen;
+	}
+	
+	public void setFullScreen(boolean fullScreen) {
+		this.fullScreen = fullScreen;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public Color getBackgroundColor() {
+		return backgroundColor;
+	}
+
+	public void setBackgroundColor(Color backgroundColor) {
+		this.backgroundColor = backgroundColor;
+	}
+
+	public boolean isCentered() {
+		return centered;
+	}
+
+	public void setCentered(boolean centered) {
+		this.centered = centered;
+	}
+
+	public void openWindow() {
+		fenster = new JFrame("JFrame");
+		fenster.getContentPane().setBackground(backgroundColor);
+		fenster.setCursor(new Cursor(1));
+		if (width == 0) {
+			width = getScreenWidth() * 10 / 9;
+		}
+		if (height == 0) {
+			height = getScreenHeight() * 10 / 9;
+		}
+		fenster.setSize(width, height);
+		if(centered) {
+			fenster.setLocationRelativeTo(null);
+		}
+		if(fullScreen) {
+			fenster.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		}
+		fenster.setVisible(true);
+	}
+
+	private int getScreenWidth() {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		return (int) screenSize.getWidth();
+	}
+
+	private int getScreenHeight() {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		return (int) screenSize.getHeight();
+	}
+
+	public void closeWindow() {
+		if(fenster.isVisible()) {
+			fenster.setVisible(false);
+		}
+	}
+	
+	public void addJLabel(JLabel jlabel) {
+		fenster.add(jlabel);
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JLabel;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Fenster f = new Fenster(0, 0, Color.RED, true, true);
+		f.openWindow();
+		JLabel j = new JLabel("Seavas");
+		j.setFont(new Font("Arial", 0, 20));
+		j.setBounds(100, 100, 100, 100);
+		f.addJLabel(j);
 	}
-
 }
