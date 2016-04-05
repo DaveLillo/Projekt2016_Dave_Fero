8a13f8d789682de227e1fb269951de8121aba089
DukeFrank <svo17072@spengergasse.at>
2016-04-05 15:54:24 +0200

    Head geändert Keys button hinzugefügt

--- src/gameState/MenuState.java
+++ src/gameState/MenuState.java
@@ -16,7 +16,7 @@ public class MenuState extends GameState {
 	private BufferedImage head;
 
 	private int currentChoice = 0;
-	private String[] options = { "Start", "Quit" };
+	private String[] options = { "Start", "Keys", "Quit" };
 
 	private Color titleColor;
 	private Font titleFont;
@@ -82,14 +82,17 @@ public class MenuState extends GameState {
 		// draw menu options
 		g.setFont(font);
 		g.setColor(Color.WHITE);
-		g.drawString("Start", 80, 200);
-		g.drawString("Quit", 200, 200);
+		g.drawString("Start", 50, 200);
+		g.drawString("Keys", 140, 200);
+		g.drawString("Quit", 230, 200);
 
 		// draw floating head
 		if (currentChoice == 0)
-			g.drawImage(head, 125, 154, null);
+			g.drawImage(head, 51, 165, null);
 		else if (currentChoice == 1)
-			g.drawImage(head, 125, 174, null);
+			g.drawImage(head, 144, 165, null);
+		else if (currentChoice == 2)
+			g.drawImage(head, 233, 165, null);
 
 		// other
 		g.setFont(font2);
@@ -104,20 +107,21 @@ public class MenuState extends GameState {
 			// gsm.setState(GameStateManager.LEVEL1ASTATE);
 			System.out.println("Start");
 		} else if (currentChoice == 1) {
+			System.out.println("Right key and Left key");
+		} else if (currentChoice == 2)
 			System.exit(0);
-		}
 	}
 
 	public void handleInput() {
 		if (Keys.isPressed(Keys.ENTER))
 			select();
-		if (Keys.isPressed(Keys.UP)) {
+		if (Keys.isPressed(Keys.LEFT)) {
 			if (currentChoice > 0) {
 				// JukeBox.play("menuoption", 0);
 				currentChoice--;
 			}
 		}
-		if (Keys.isPressed(Keys.DOWN)) {
+		if (Keys.isPressed(Keys.RIGHT)) {
 			if (currentChoice < options.length - 1) {
 				// JukeBox.play("menuoption", 0);
 				currentChoice++;
